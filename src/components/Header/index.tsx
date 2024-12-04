e5ca437a0746101a1aece61ef6325743ced97043
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-04 17:04:54 -0300

    Cria Css para o header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
+import './styles.css';
+import logoImg from '../../images/logo-sistema-solar.svg';
+
 function Header() {
   return (
     <header>
-      <h1>Sistema Solar</h1>
+      {/* <h1>Sistema Solar</h1> */}
+      <img src={ logoImg } alt="" />
+
     </header>
   );
 }
