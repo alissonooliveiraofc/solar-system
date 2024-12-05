76e49b95eff4a9cb2282d3ea575a3057a1dbaccd
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-05 18:29:52 -0300

    Finaliza CSS

--- src/components/MissionCard/index.tsx
+++ src/components/MissionCard/index.tsx
@@ -9,9 +9,21 @@ function MissionCard({ name, year, country, destination }: MissionCardProps) {
   return (
     <div className="card-container2" data-testid="mission-card">
       <p data-testid="mission-name">{name}</p>
-      <p data-testid="mission-year">{year}</p>
-      <p data-testid="mission-country">{country}</p>
-      <p data-testid="mission-destination">{destination}</p>
+
+      <p data-testid="mission-year">
+        <img className="icon" src="src/images/schedule.svg" alt="calendário" />
+        {year}
+      </p>
+
+      <p data-testid="mission-country">
+        <img className="icon" src="src/images/map.svg" alt="maps" />
+        {country}
+      </p>
+
+      <p data-testid="mission-destination">
+        <img className="icon" src="src/images/objective.svg" alt="destination" />
+        {destination}
+      </p>
     </div>
   );
 }
