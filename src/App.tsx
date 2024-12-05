4103aaba19c837948dca01756ef2b3a3852ae847
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-12-04 22:47:20 -0300

    Req 7 - Cria componente Missions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <div className="line" />
+      {/* <div className="line" /> */}
       <SolarSystem />
+      <Missions />
     </>
   );
 }
